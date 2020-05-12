f92c81c0dc6e5f4b11c93cc018052b5330ff7df6
Arnur Kupanov <ncase01@gmail.com>
2020-05-12 19:11:33 +0600

    added formatting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const chalk = require('chalk');
 
 const { lstat } = fs.promises;
 
@@ -8,10 +9,13 @@ fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) throw new Error(err);
 
   const allPromises = filenames.map((filename) => lstat(filename));
-
   const allStats = await Promise.all(allPromises);
 
   allStats.forEach((stats, index) => {
-    console.log(filenames[index], stats.isFile());
+    if (stats.isFile()) {
+      console.log(chalk.underline(filenames[index]));
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
   });
 });
